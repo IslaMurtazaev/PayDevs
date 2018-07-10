0b03e42d75ad327707bc2febe16954701cd3d148
Islam Murtazaev <islam.murtazaev@gmail.com>
2018-07-10 14:31:01 +0600

    remove garbage

--- PayDevs/decorators.py
+++ PayDevs/decorators.py
@@ -1,6 +1,6 @@
-from example_paydevs.exceptions import EntityDoesNotExistException, NoPermissionException, ExampleException, \
+from PayDevs.exceptions import EntityDoesNotExistException, NoPermissionException, PayDevsException, \
     EntityIntegrityException, InvalidEntityException
-from example_paydevs.serializer import ExampleExceptionSerializer
+from PayDevs.serializer import ExampleExceptionSerializer
 
 exception_status_code = {
     EntityDoesNotExistException: 404,
@@ -14,7 +14,7 @@ def serialize_exception(method):
     def method_wrapper(*args, **kwargs):
         try:
             return method(*args, **kwargs)
-        except ExampleException as e:
+        except PayDevsException as e:
             ExampleExceptionSerializer.model = e.__class__
             body = ExampleExceptionSerializer.serializer(e)
             status = exception_status_code[type(e)]
