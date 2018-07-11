4f20c466e0346719e295bfa4e7d298ae38cbdbd7
Nursultan1125 <zholnurs@gmail.com>
2018-07-11 15:04:58 +0600

    fix views

--- PayDevs/views.py
+++ PayDevs/views.py
@@ -10,11 +10,13 @@ class ViewWrapper(View):
     view_factory = None
 
     def get(self, request, *args, **kwargs):
-        body, status = self.view_factory().get(**kwargs)
+        kwargs.update(request.POST.dict())
+        body, status = self.view_factory().get(*args, **kwargs)
         return HttpResponse(json.dumps(body), status=status, content_type='application/json')
 
     def post(self, request, *args, **kwargs):
-        body, status = self.view_factory().post(request, *args, **kwargs)
+        kwargs.update(request.POST.dict())
+        body, status = self.view_factory().post(*args, **kwargs)
         return HttpResponse(json.dumps(body), status=status, content_type='application/json')
 
 
