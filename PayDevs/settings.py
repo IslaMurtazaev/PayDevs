9eefa18b56bc1f2247cc82afec809df0ecd3499e
Islam Murtazaev <islam.murtazaev@gmail.com>
2018-07-12 15:20:44 +0600

    Add models and entities to project

--- PayDevs/settings.py
+++ PayDevs/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-    'account'
+    'account',
+    'project'
 ]
 
 MIDDLEWARE = [
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
 
@@ -120,11 +120,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-
-
-try:
-    import PayDevs.my_local
-
-except:
-    pass
