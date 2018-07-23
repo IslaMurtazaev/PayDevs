40f117995352bcdecb161f0c5a138f0ef402eb35
Islam Murtazaev <islam.murtazaev@gmail.com>
2018-07-23 13:03:55 +0600

    Add PDF converter to project bill

--- PayDevs/views.py
+++ PayDevs/views.py
@@ -2,6 +2,7 @@ import json
 
 from django.http import HttpResponse
 from django.views import View
+from reportlab.pdfgen import canvas
 
 from account.factories import AuthUserInteractorFactory
 from PayDevs import settings
@@ -15,7 +16,11 @@ class ViewWrapper(View):
         kwargs.update(request.GET.dict())
         kwargs.update(self.params(request))
         body, status = self.view_factory().get(*args, **kwargs)
-        return HttpResponse(json.dumps(body), status=status, content_type='application/json')
+
+        if body.get('total'):
+            return self.pdf_generator(body)
+        else:
+            return HttpResponse(json.dumps(body), status=status, content_type='application/json')
 
 
     def post(self, request, *args, **kwargs):
@@ -63,6 +68,31 @@ class ViewWrapper(View):
         logged_id = AuthUserInteractorFactory().create().set_params(token=token,
                                                                     secret_key=settings.SECRET_KEY).execute()
         return logged_id
+
+
+    def pdf_generator(self, body):
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = 'attachment; filename="bill.pdf"'
+
+        p = canvas.Canvas(response)
+        height = 700
+        p.drawString(200, 800, "Project total report bill")
+        for key in body:
+            if key == 'type_of_payment':
+                if (body[key] == 'M_P'):
+                    body[key] = "Monthly payment"
+                elif (body[key] == 'H_P'):
+                    body[key] = "Hourly payment"
+                else:
+                    body[key] = "Taskly payment"
+                p.drawString(150, height, "Type of payment: " + body[key])
+            else:
+                p.drawString(150, height, "%s:  %s" % (key.title(), body[key]))
+            height -= 25
+        p.showPage()
+        p.save()
+
+        return response
 
 
     def params(self, request):
