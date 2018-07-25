90a1ea68cbc92d7f3c33a55df356e322f05bec6a
Nursultan1125 <zholnurs@gmail.com>
2018-07-25 11:06:02 +0600

    modified view

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from account.serializers import UserSerializer, UserListSerializer
 from PayDevs.decorators import serialize_exception
+from PayDevs.constants import StatusCodes
 
 
 class UserView(object):
@@ -11,7 +12,7 @@ class UserView(object):
         user = self.get_user_interactor.set_params(id=kwargs['logged_id']).execute()
         body = UserSerializer.serialize(user)
 
-        status = 200
+        status = StatusCodes.OK
         return body, status
 
 
@@ -27,7 +28,7 @@ class UserRegisterView(object):
         user = self.auth(**kwargs)
         body = UserSerializer.serialize(user)
         body.update({'token': user.token})
-        status = 200
+        status = StatusCodes.CREATED
         return body, status
 
     def auth(self, username, password, secret_key=None, **kwargs):
@@ -44,5 +45,5 @@ class LoginUserView(object):
         user = self.get_user_interactor.set_params(**kwargs).execute()
         body = UserSerializer.serialize(user)
         body.update({'token': user.token})
-        status = 200
+        status = StatusCodes.OK
         return body, status
