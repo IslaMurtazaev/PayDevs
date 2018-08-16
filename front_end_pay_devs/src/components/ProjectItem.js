33faef0acddf8a7d8b2fc49f9fe9afaf3fee19fa
Islam Murtazaev <islam.murtazaev@gmail.com>
2018-08-16 15:11:08 +0600

    Change outlook of 'ProjectItem'

--- front_end_pay_devs/src/components/ProjectItem.js
+++ front_end_pay_devs/src/components/ProjectItem.js
@@ -10,16 +10,35 @@ class ProjectItem extends Component {
 
   render() {
     let project = this.props.project;
-    console.log("first", project);
     if (!project) return <Redirect from="/project/:id" to="/" />;
+
+    let type_of_payment;
+    switch(project.type_of_payment) {
+      case "M_P":
+        type_of_payment = "Monthly";
+        break;
+      case "H_P":
+        type_of_payment = "Hourly";
+        break;
+      case "T_P":
+        type_of_payment = "Taskly";
+        break
+    }
     return (
-      <div>
-        <h2>{project.title}</h2>
-        <p>Description: {project.description}</p>
-        <p>Type of payment: {project.type_of_payment}</p>
-        <p>Start date: {new Date(project.start_date).toString()}</p>
-        <p>End date: {new Date(project.end_date).toString()}</p>
-        <button onClick={this.onClick.bind(this, project.id)}>
+        <div className="container">
+        <h3>Title: {project.title}</h3>
+        {project.description ? (
+          <h4>Description: {project.description}</h4>
+        ) : null}
+        {project.start_date ? (
+          <h4>Start-date: {new Date(project.start_date).toDateString()}</h4>
+        ) : null}
+        {project.end_date ? (
+          <h4>End-date: {new Date(project.end_date).toDateString()}</h4>
+        ) : null}
+        <h4>Type of payment: {type_of_payment}</h4>
+        <h4>Status: {project.status ? "" : "not"} active</h4>
+        <button className="btn btn-danger" onClick={this.onClick.bind(this, project.id)}>
           DELETE PROJECT
         </button>
       </div>
