98f9fa5fc1ba31274ecf45f91f4278b02ebfd5b8
Islam Murtazaev <islam.murtazaev@gmail.com>
2018-08-21 13:58:52 +0600

    Edit AppRouter

--- front_end_pay_devs/src/route/AppRouter.js
+++ front_end_pay_devs/src/route/AppRouter.js
@@ -10,13 +10,10 @@ import { PrivateRoute } from "./PrivateRoute";
 import { history } from "../index";
 import CreateProjectForm from "../components/Project/CreateProjectForm";
 import UpdateProjectForm from "../components/Project/UpdateProjectForm";
-import CreateTaskForm from "../components/Task/CreateTask";
-import CreateMonthPaymentForm from "../components/MonthPayment/CreateMonthPaymentForm";
 import NotFound from "../components/NotFound";
 import UpdateTaskForm from "../components/Task/UpdateTaskForm";
 import UpdateWorkedDayForm from "../components/WorkedDay/UpdateWorkedDayForm";
 import WorkedDays from "../components/WorkedDay/WorkedDays";
-import CreateHourPayment from "../components/HourPayment/CreateHourPayment";
 import UpdateHourlyForm from "../components/HourPayment/UpdateHourlyForm";
 import WorkTimes from "../components/WorkTime/WorkTimes";
 import UpdateWorkTimeForm from "../components/WorkTime/UpdateWorkTimeForm";
@@ -31,11 +28,8 @@ class AppRouter extends Component {
           <Route path="/sign_up" component={SignUp} />
           <PrivateRoute path="/project/create" component={CreateProjectForm} />
           <PrivateRoute path="/project/:id/Taskly/:taskId/update" component={UpdateTaskForm}/>
-          <PrivateRoute path="/project/:id/Taskly/create" component={CreateTaskForm}/>
-          <PrivateRoute path="/project/:id/Monthly/create" component={CreateMonthPaymentForm} />
           <PrivateRoute path="/project/:id/Monthly/:monthPaymentId/workedDay/:workedDayId/update" component={UpdateWorkedDayForm} />
           <PrivateRoute path="/project/:id/Monthly/:monthPaymentId/workedDay" component={WorkedDays} />
-          <PrivateRoute path="/project/:id/Hourly/create" component={CreateHourPayment} />
           <PrivateRoute path="/project/:id/Hourly/:hourPaymentId/update" component={UpdateHourlyForm} />
           <PrivateRoute path="/project/:id/Hourly/:hourPaymentId/workTime/:workTimeId/update" component={UpdateWorkTimeForm} />
           <PrivateRoute path="/project/:id/Hourly/:hourPaymentId/workTime" component={WorkTimes} />
