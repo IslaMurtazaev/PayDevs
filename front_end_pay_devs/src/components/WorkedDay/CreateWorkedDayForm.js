a34154e52445583c3dabcef05aa6b31ff11d72bd
Zhanzat <janzat_98@mail.ru>
2018-08-22 08:17:14 +0600

    Fix conflicts

--- front_end_pay_devs/src/components/WorkedDay/CreateWorkedDayForm.js
+++ front_end_pay_devs/src/components/WorkedDay/CreateWorkedDayForm.js
@@ -7,12 +7,8 @@ import workedDayActions from "../../actions/workedDay";
 const CreateWorkedDayForm = props => {
   return (
     <div>
-<<<<<<< HEAD
       <h3><b>New Worked Day</b></h3>
-=======
-      <h4>Create Worked Day</h4>
->>>>>>> 3f3c0041ff453f04a3f7cc836bf62aaa25342d9d
-      <FormikWorkedDay onSubmit={props.createWorkedDay} />
+    <FormikWorkedDay onSubmit={props.createWorkedDay} />
     </div>
   );
 };
