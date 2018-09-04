9f9311638bdecda4dcef4f25a3244b0f41de91b7
Nursultan1125 <zholnurs@gmail.com>
2018-09-04 10:54:10 +0600

    modified mock axios

--- front_end_pay_devs/src/__mocks__/axios.js
+++ front_end_pay_devs/src/__mocks__/axios.js
@@ -1,71 +1,73 @@
 export default {
-    
-    get: jest.fn((url) => {
-        return Promise.resolve({
-            data: {}
-        });
-    
-    }),
-    put: jest.fn((url) => {
-        return Promise.resolve({
-            data: {}
-        });
-    
-    }),
-    delete: jest.fn((url) => {
-        return Promise.resolve({
-            data: {}
-        });
-    
-    }),
-    post: jest.fn((url, data) => {
-        var parser = document.createElement('a');
-        parser.href = url;
-        let pathList = parser.pathname.split("/")
-        
-        // let urlList = url.split('/')
-        if(pathList.indexOf('users') !== -1){
-            return userAxiosRender(pathList, data);
-        }
-        console.log(url)
-        return Promise.resolve({ data: {} })
-          
-    }),
-    create: jest.fn(function () {
-        return this;
-    }),
-    defaults: jest.fn((url) => {
-        return Promise.resolve({
-            data: {}
-        });
-    
-    }),
+  get: jest.fn(url => {
+    return Promise.resolve({
+      data: {}
+    });
+  }),
+  put: jest.fn(url => {
+    return Promise.resolve({
+      data: {}
+    });
+  }),
+  delete: jest.fn(url => {
+    return Promise.resolve({
+      data: {}
+    });
+  }),
+  post: jest.fn((url, data) => {
+    var parser = document.createElement("a");
+    parser.href = url;
+    let pathList = parser.pathname.split("/");
 
-    
+    // let urlList = url.split('/')
+    if (pathList.indexOf("users") !== -1) {
+      return userAxiosRender(pathList, data);
+    }
+    return Promise.resolve({ data: {} });
+  }),
+  create: jest.fn(function() {
+    return this;
+  }),
+  defaults: jest.fn(url => {
+    return Promise.resolve({
+      data: {}
+    });
+  })
 };
 
-function userAxiosRender(pathList, data){
-    const user = JSON.parse(data)
-    const path = pathList.pop()
-    if(path === 'login'){
-        if(user.username === 'test_user_name' && user.username === 'test_user_name')
-            return Promise.resolve({ data: {
-                username: user.username, 
-                email: 'test@gmail.com',
-                token: "__TOKEN__"
-            } })
-        else{
-            return Promise.resolve({ data: {error: {
-                source: "entity",
-                code: "not_found",
-                message: "Entity not found"
-            } }})
+function userAxiosRender(pathList, data) {
+  const user = JSON.parse(data);
+  const path = pathList.pop();
+  if (path === "login") {
+    if (
+      user.username === "test_user_name" &&
+      user.username === "test_user_name"
+    )
+      return Promise.resolve({
+        data: {
+          username: user.username,
+          email: "test@gmail.com",
+          token: "__TOKEN__"
         }
-    }else if (path === 'create'){
-        return Promise.resolve({ data: {
-            username: user.username, 
-            email: user.email,
-            token: "__TOKEN__"
-        } })
+      });
+    else {
+      return Promise.resolve({
+        data: {
+          error: {
+            source: "entity",
+            code: "not_found",
+            message: "Entity not found"
+          }
+        }
+      });
     }
+  } else if (path === "create") {
+    return Promise.resolve({
+      data: {
+        username: user.username,
+        email: user.email,
+        token: "__TOKEN__"
+      }
+    });
+  }
 }
