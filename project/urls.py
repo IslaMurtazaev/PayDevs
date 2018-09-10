6494054475ba7b826334df3588a1747e923a1007
Nursultan1125 <zholnurs@gmail.com>
2018-09-10 16:33:09 +0600

    modified url Django ViewWrapper

--- project/urls.py
+++ project/urls.py
@@ -8,11 +8,11 @@ urlpatterns = [
 
     path('create', ViewWrapper.as_view(view_factory=create_project_factory), name='create_project'),
 
-    path('<int:project_id>/', ViewWrapper.as_view(view_factory=get_project_factory), name='get_project'),
+    path('<int:project_id>', ViewWrapper.as_view(view_factory=get_project_factory), name='get_project'),
 
-    path('<int:project_id>/update/', ViewWrapper.as_view(view_factory=update_project_factory), name='update_project'),
+    path('<int:project_id>/update', ViewWrapper.as_view(view_factory=update_project_factory), name='update_project'),
 
-    path('<int:project_id>/delete/', ViewWrapper.as_view(view_factory=delete_project_factory), name='delete_project'),
+    path('<int:project_id>/delete', ViewWrapper.as_view(view_factory=delete_project_factory), name='delete_project'),
 
     path('all', ViewWrapper.as_view(view_factory=get_projects_all_factory), name='get_all_projects'),
 
@@ -20,70 +20,70 @@ urlpatterns = [
 
 
 
-    path('<int:project_id>/task/create/', ViewWrapper.as_view(
+    path('<int:project_id>/task/create', ViewWrapper.as_view(
         view_factory=create_task_factory), name='create_task'),
 
-    path('<int:project_id>/task/<int:task_id>/', ViewWrapper.as_view(
+    path('<int:project_id>/task/<int:task_id>', ViewWrapper.as_view(
         view_factory=get_task_factory), name='get_task'),
 
-    path('<int:project_id>/task/<int:task_id>/update/', ViewWrapper.as_view(
+    path('<int:project_id>/task/<int:task_id>/update', ViewWrapper.as_view(
         view_factory=update_task_factory), name='update_task'),
 
-    path('task/<int:task_id>/delete/', ViewWrapper.as_view(
+    path('task/<int:task_id>/delete', ViewWrapper.as_view(
         view_factory=delete_task_factory), name='delete_task'),
 
-    path('<int:project_id>/task/all/', ViewWrapper.as_view(
+    path('<int:project_id>/task/all', ViewWrapper.as_view(
         view_factory=get_all_tasks_factory), name='get_all_tasks'),
 
 
 
-    path('<int:project_id>/month_payment/create/', ViewWrapper.as_view(
+    path('<int:project_id>/month_payment/create', ViewWrapper.as_view(
         view_factory=create_month_payment_factory), name='create_month_payment'),
 
-    path('month_payment/<int:month_payment_id>/', ViewWrapper.as_view(
+    path('month_payment/<int:month_payment_id>', ViewWrapper.as_view(
         view_factory=get_month_payment_factory), name='get_month_payment'),
 
-    path('<int:project_id>/month_payment/<int:month_payment_id>/update/',
+    path('<int:project_id>/month_payment/<int:month_payment_id>/update',
          ViewWrapper.as_view(view_factory=update_month_payment_factory), name='update_month_payment'),
 
-    path('month_payment/<int:month_payment_id>/delete/',
+    path('month_payment/<int:month_payment_id>/delete',
          ViewWrapper.as_view(view_factory=delete_month_payment_factory), name='delete_month_payment'),
 
-    path('<int:project_id>/month_payment/all/',
+    path('<int:project_id>/month_payment/all',
          ViewWrapper.as_view(view_factory=get_all_month_payments_factory), name='get_all_month_payments'),
 
 
 
-    path('<int:project_id>/month_payment/<int:month_payment_id>/worked_day/create/',
+    path('<int:project_id>/month_payment/<int:month_payment_id>/worked_day/create',
          ViewWrapper.as_view(view_factory=create_worked_day_factory), name='create_worked_day'),
 
-    path('worked_day/<int:worked_day_id>/',
+    path('worked_day/<int:worked_day_id>',
          ViewWrapper.as_view(view_factory=get_worked_day_factory), name='get_worked_day'),
 
-    path('<int:project_id>/month_payment/<int:month_payment_id>/worked_day/<int:worked_day_id>/update/',
+    path('<int:project_id>/month_payment/<int:month_payment_id>/worked_day/<int:worked_day_id>/update',
          ViewWrapper.as_view(view_factory=update_worked_day_factory), name='update_worked_day'),
 
-    path('worked_day/<int:worked_day_id>/delete/',
+    path('worked_day/<int:worked_day_id>/delete',
          ViewWrapper.as_view(view_factory=delete_worked_day_factory), name='delete_worked_day'),
 
-    path('month_payment/<int:month_payment_id>/worked_day/all/',
+    path('month_payment/<int:month_payment_id>/worked_day/all',
          ViewWrapper.as_view(view_factory=get_all_worked_days_factory), name='get_all_worked_days'),
 
 
 
-    path('<int:project_id>/hour_payment/create/',
+    path('<int:project_id>/hour_payment/create',
          ViewWrapper.as_view(view_factory=create_hour_payment_factory), name='create_hour_payment'),
 
-    path('hour_payment/<int:hour_payment_id>/',
+    path('hour_payment/<int:hour_payment_id>',
          ViewWrapper.as_view(view_factory=get_hour_payment_factory), name='get_hour_payment'),
 
-    path('<int:project_id>/hour_payment/<int:hour_payment_id>/update/',
+    path('<int:project_id>/hour_payment/<int:hour_payment_id>/update',
          ViewWrapper.as_view(view_factory=update_hour_payment_factory), name='update_hour_payment'),
 
-    path('hour_payment/<int:hour_payment_id>/delete/',
+    path('hour_payment/<int:hour_payment_id>/delete',
          ViewWrapper.as_view(view_factory=delete_hour_payment_factory), name='delete_hour_payment'),
 
-    path('<int:project_id>/hour_payment/all/',
+    path('<int:project_id>/hour_payment/all',
          ViewWrapper.as_view(view_factory=get_all_hour_payment_factory), name='get_all_hour_payments'),
 
 
